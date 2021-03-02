aa9fce289ac866a65ae21156ba7c4a29ee2a40dc
Karl Koch <hi@kejk.co>
2021-03-02 16:52:21 +0000

    fix: added ability to change independent corners if padding isn't equal

--- code.ts
+++ code.ts
@@ -10,15 +10,34 @@ figma.ui.onmessage = (msg) => {
       for (node of selection) {
         if (nodeTypes.includes(node.type)) {
           for (let innerN of node.children) {
-            if (node.paddingLeft !== node.paddingTop) {
-              figma.notify("Padding values must be equal");
-            }
-            if (childNodeTypes.includes(node.type)) {
-              if (node.paddingLeft === node.paddingTop) {
-                let innerRadius = Number(innerN.cornerRadius);
-                let paddingValue = Number(node.paddingTop);
-                node.cornerRadius = innerRadius + paddingValue;
-                figma.notify(`Outer radius set to ${node.cornerRadius}!`);
+            let innerRadius = Number(innerN.cornerRadius);
+            let paddingValue = Number(node.paddingTop);
+            if (
+              node.paddingLeft === paddingValue &&
+              node.paddingLeft === node.paddingBottom
+            ) {
+              node.cornerRadius = innerRadius + paddingValue;
+              figma.notify(`Outer radius set to ${node.cornerRadius}!`);
+            } else if (
+              node.paddingLeft === paddingValue &&
+              node.paddingLeft !== node.paddingBottom
+            ) {
+              node.topLeftRadius = innerRadius + paddingValue;
+              node.topRightRadius = innerRadius + paddingValue;
+              figma.notify(`Outer radius set to ${node.topLeftRadius}!`);
+            } else if (
+              node.paddingLeft !== paddingValue &&
+              node.paddingLeft === node.paddingBottom
+            ) {
+              node.bottomLeftRadius = innerRadius + paddingValue;
+              node.bottomRightRadius = innerRadius + paddingValue;
+              figma.notify(`Outer radius set to ${node.bottomLeftRadius}!`);
+            } else {
+              if (
+                node.paddingLeft !== paddingValue ||
+                node.paddingLeft !== node.paddingBottom
+              ) {
+                figma.notify("Padding values must be equal");
               }
             }
           }
@@ -34,22 +53,36 @@ figma.ui.onmessage = (msg) => {
       for (node of selection) {
         if (nodeTypes.includes(node.type)) {
           for (let innerN of node.children) {
-            if (node.paddingLeft !== node.paddingTop) {
-              figma.notify("Padding values must be equal");
-            }
             if (childNodeTypes.includes(node.type)) {
               let outerRadius = Number(node.cornerRadius);
               let paddingValue = Number(node.paddingTop);
               if (
-                paddingValue >= outerRadius &&
-                node.paddingLeft === node.paddingTop
+                node.paddingLeft === node.paddingTop &&
+                node.paddingLeft === node.paddingBottom
               ) {
-                figma.notify(
-                  "The padding size is too large to require a border radius on the inner element"
-                );
-              } else if (node.paddingLeft === node.paddingTop) {
                 innerN.cornerRadius = outerRadius - paddingValue;
-                figma.notify(`Inner radius set to ${innerN.cornerRadius}!`);
+                figma.notify(`Outer radius set to ${innerN.cornerRadius}!`);
+              } else if (
+                node.paddingLeft === node.paddingTop &&
+                node.paddingLeft !== node.paddingBottom
+              ) {
+                innerN.topLeftRadius = outerRadius - paddingValue;
+                innerN.topRightRadius = outerRadius - paddingValue;
+                figma.notify(`Outer radius set to ${innerN.topLeftRadius}!`);
+              } else if (
+                node.paddingLeft !== node.paddingTop &&
+                node.paddingLeft === node.paddingBottom
+              ) {
+                innerN.bottomLeftRadius = outerRadius - paddingValue;
+                innerN.bottomRightRadius = outerRadius - paddingValue;
+                figma.notify(`Outer radius set to ${innerN.bottomLeftRadius}!`);
+              } else {
+                if (
+                  node.paddingLeft !== node.paddingTop ||
+                  node.paddingLeft !== node.paddingBottom
+                ) {
+                  figma.notify("Padding values must be equal");
+                }
               }
             }
           }
